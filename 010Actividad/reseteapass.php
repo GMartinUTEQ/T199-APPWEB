6e0f20a39257a897824bb4d8707216757dfbb6b6
GMartinMx78UTEQ <gabriel.martin@uteq.edu.mx>
2021-06-03 08:11:02 -0500

    u

--- 010Actividad/reseteapass.php
+++ 010Actividad/reseteapass.php
@@ -6,7 +6,7 @@
     </head>
     <body>
         <h1>Resetea tu contraseña</h1>
-        <form action="resetapass.php" method="post">
+        <form action="reseteapass.php" method="post">
             <table style="width:50%">
                 <tr>
                     <td>
@@ -41,3 +41,54 @@
         </form>
     </body>
 </html>
+<?php
+
+    //insertando los datos de mi conexión.
+    include("conexion.php");
+
+    //Validando que si me hayan llegado los campos que ocupo.
+    if(isset($_REQUEST["usrpassuno"]) && isset($_REQUEST["usrpassdos"]))
+    {
+        //Asignando la información que me llegó del post a variables.
+        $usralias = $_REQUEST["usralias"];
+        $usrpass1 = $_REQUEST["usrpassuno"];
+        $usrpass2 = $_REQUEST["usrpassdos"];
+
+        //Validando que las contraseñas no estén vacías, es decir que si se hayan escrito.
+        if(!empty($usrpass1) && !empty($usrpass2))
+        {
+            //Validar que las contraseñas sean iguales
+            if($usrpass1 === $usrpass2)
+            {
+                //Validar que no tenga errores de conexión a MySQL
+                if($conn->connect_error)
+                {
+                    echo "Error al conectar a MySQL" . $conn->connect_error;
+                    die("");
+                }
+
+                //Solo estoy llenando la cadena con el query, no la estoy ejecutando.
+                $cadenamysql = "update usuario set pass = md5('$usrpass1'), ultcambio = now() where alias = '$usralias';";
+                
+                //Ejecutar la consulta y validar que no haya dado error en MySQL
+                if($conn->query($cadenamysql) === TRUE)
+                {
+                    echo "<h1>Contraseña Actualizada</h1>";
+                }
+                else 
+                {
+                    echo "Error al modificar la contraseña: " . $conn->error;
+                }
+            }
+            else
+            {
+                echo "Las contraseñas capturadas no son iguales";
+            }
+        }
+        else
+        {
+            echo "Favor de escribir las contraseñas";
+        }
+    }
+
+?>
