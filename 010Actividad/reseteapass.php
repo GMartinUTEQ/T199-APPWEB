f76e95d755c7c615f02418f9b84e6d5f8cf7e32d
GMartinMx78UTEQ <gabriel.martin@uteq.edu.mx>
2021-06-03 07:47:31 -0500

    g

---
+++ 010Actividad/reseteapass.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Resetea tu contraseña</title>
+    </head>
+    <body>
+        <h1>Resetea tu contraseña</h1>
+        <form action="resetapass.php" method="post">
+            
+        </form>
+    </body>
+</html>
