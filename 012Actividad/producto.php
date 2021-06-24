138a01e221241f4b5e932ba37a824fcd57877223
GMartinMx78UTEQ <gabriel.martin@uteq.edu.mx>
2021-06-24 08:21:54 -0500

    Producto de bd

--- 012Actividad/producto.php
+++ 012Actividad/producto.php
@@ -54,7 +54,12 @@
                         die("");
                     }
 
-                    $cadena = "select * from producto";
+                    if(!isset($_REQUEST["idp"]))
+                    {
+                        echo "<script>window.location='index.php';</script>";
+                    }
+
+                    $cadena = "select * from producto where idproducto =" . $_REQUEST["idp"];
                     
                     $resultado = $conn->query($cadena);
 
@@ -63,7 +68,29 @@
                         
                         while($row = $resultado->fetch_assoc())
                         {
-                            echo "";
+                            echo '<section class="py-5">
+                                    <div class="container px-4 px-lg-5 my-5">
+                                        <div class="row gx-4 gx-lg-5 align-items-center">
+                                            <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0" src="' . $row["urlproducto"] . '" alt="..." /></div>
+                                            <div class="col-md-6">
+                                                <div class="small mb-1">SKU: ' . $row["idproducto"] . '</div>
+                                                <h1 class="display-5 fw-bolder">' . $row["nombreproducto"] . '</h1>
+                                                <div class="fs-5 mb-5">
+                                                    <span class="text-decoration-line-through">$' . number_format((float)($row["precioproducto"] * 1.18), 2, '.', '') . '</span>
+                                                    <span>$'. number_format((float)$row["precioproducto"], 2, '.', '') .'</span>
+                                                </div>
+                                                <p class="lead">'. $row["descripcionproducto"] . '</p>
+                                                <div class="d-flex">
+                                                    <input class="form-control text-center me-3" min="' . 0 . '" max="' . $row["inventarioproducto"] . '" id="inputQuantity" type="num" value="1" style="max-width: 3rem" />
+                                                    <button class="btn btn-outline-dark flex-shrink-0" type="button">
+                                                        <i class="bi-cart-fill me-1"></i>
+                                                        Add to cart
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </section>';
                         }
                     }
                     else
@@ -75,29 +102,7 @@
 
 
 
-        <section class="py-5">
-            <div class="container px-4 px-lg-5 my-5">
-                <div class="row gx-4 gx-lg-5 align-items-center">
-                    <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0" src="https://dummyimage.com/600x700/dee2e6/6c757d.jpg" alt="..." /></div>
-                    <div class="col-md-6">
-                        <div class="small mb-1">SKU: BST-498</div>
-                        <h1 class="display-5 fw-bolder">Shop item template</h1>
-                        <div class="fs-5 mb-5">
-                            <span class="text-decoration-line-through">$45.00</span>
-                            <span>$40.00</span>
-                        </div>
-                        <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium at dolorem quidem modi. Nam sequi consequatur obcaecati excepturi alias magni, accusamus eius blanditiis delectus ipsam minima ea iste laborum vero?</p>
-                        <div class="d-flex">
-                            <input class="form-control text-center me-3" id="inputQuantity" type="num" value="1" style="max-width: 3rem" />
-                            <button class="btn btn-outline-dark flex-shrink-0" type="button">
-                                <i class="bi-cart-fill me-1"></i>
-                                Add to cart
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
+        
         <!-- Related items section-->
         <section class="py-5 bg-light">
             <div class="container px-4 px-lg-5 mt-5">
