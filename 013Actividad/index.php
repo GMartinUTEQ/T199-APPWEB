3313c4d413cc699366e3e7a843b5d1a1f7889c07
GMartin <gabosoft@gmail.com>
2021-07-20 14:03:01 -0500

    optionsfrommysql

--- 013Actividad/index.php
+++ 013Actividad/index.php
@@ -1,29 +1,103 @@
 <html>
     <head>
-        <title>Iput Select</title>
+        <title>Input Select</title>
     </head> 
     <body>
-        <form action="altaproducto.php" method="post">
+        <?php
+            $targetAction="altaproducto.php";
+            $btnSubmit="Guardar";
+            $idpro = "";
+            $nombrepro ="";
+            $preciopro="";
+            $idtalla = 0;
+            $idmarca = 0;
+            if(isset($_REQUEST["idpro"]))
+            {
+                //Entonces es una edición
+                $targetAction="editaproducto.php";
+                $btnSubmit="Actualizar";
+
+                include("conexion.php");
+
+                if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+                }
+
+                $sql = "select idproducto, nombre, precio, idtalla, idmarca from producto where idproducto = " . $_REQUEST["idpro"];
+                $result = $conn->query($sql);
+
+                if ($result->num_rows > 0) {
+                // output data of each row
+                while($row = $result->fetch_assoc()) {
+                    $idpro = $row["idproducto"];
+                    $nombrepro = $row["nombre"];
+                    $preciopro=$row["precio"];
+                    $idtalla = $row["idtalla"];
+                    $idmarca = $row["idmarca"];
+                }
+                } else {
+                echo "0 results";
+                }
+                $conn->close();
+            }
+
+        ?>
+        <form action="<?=$targetAction?>" method="post">
             ID Producto:<br/>
-            <input type="text" id="idpro" name="idpro" readonly/><br/>
+            <input type="text" id="idpro" name="idpro" value="<?= $idpro ?>" readonly/><br/>
             Nombre producto:<br/>
-            <input type="text" id="nompro" name="nompro"/><br/>
+            <input type="text" id="nompro" name="nompro" value="<?= $nombrepro?>"/><br/>
             Precio producto:<br/>
-            <input type="text" id="prepro" name="prepro"/><br/>
+            <input type="text" id="prepro" name="prepro" value="<?= $preciopro ?>"/><br/>
             Marca:<br/>
             <select id="marcapro" name="marcapro">
-                <option value="1">American Eeagle</option>
-                <option value="2">GAP</option>
-                <option value="3">Pineda Covalín</option>
+                <option value="0">== Seleccione una opción ==</option>
+                <?php
+                    include("conexion.php");
+
+                    if ($conn->connect_error) {
+                    die("Connection failed: " . $conn->connect_error);
+                    }
+
+                    $sql = "select * from marca";
+                    $result = $conn->query($sql);
+
+                    if ($result->num_rows > 0) {
+                    // output data of each row
+                    while($row = $result->fetch_assoc()) {
+                        echo "<option value='" . $row["idmarca"] . "'>" . $row["marca"] . "</option>";
+                    }
+                    } else {
+                    echo "0 results";
+                    }
+                    $conn->close();
+                ?>
             </select><br/>
             Talla:<br/>
             <select id="tallapro" name="tallapro">
-                <option value="1">Chica</option>
-                <option value="2">Mediana</option>
-                <option value="3">Grande</option>
-                <option value="4">Extra Grande</option>
+            <option value="0">== Seleccione una opción ==</option>
+                <?php
+                    include("conexion.php");
+
+                    if ($conn->connect_error) {
+                    die("Connection failed: " . $conn->connect_error);
+                    }
+
+                    $sql = "select * from talla";
+                    $result = $conn->query($sql);
+
+                    if ($result->num_rows > 0) {
+                    // output data of each row
+                    while($row = $result->fetch_assoc()) {
+                        echo "<option value='" . $row["idtalla"] . "'>" . $row["talla"] . "</option>";
+                    }
+                    } else {
+                    echo "0 results";
+                    }
+                    $conn->close();
+                ?>
             </select><br/>
-            <input type="submit" value="Guardar"/>
+            <input type="submit" value="<?=$btnSubmit?>"/>
         </form>
     </body>
 </html>
